8c5552f251cc957ab6867c087209aba08aa77fdc
YACINE BOUREZAK <2288656@cegepgranby.qc.ca>
2025-01-29 12:34:54 -0500

    ajout textView, EditText, Boutton dans activity_name.xml et changement de page principale pour activity_name.xml. Creation NameActivity.java

---
+++ app/src/main/java/com/example/e2_2/NameActivity.java
@@ -0,0 +1,12 @@
+package com.example.e2_2;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NameActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_name);
+    }
+}
